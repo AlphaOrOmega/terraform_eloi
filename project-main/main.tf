cdc58ffdd1a82116eea2ce6ddf96bf365c8ab319
Julien <julien.millionbalard@lilo.org>
2025-11-24 14:08:52 +0100

    step2 WORKING

--- project-main/main.tf
+++ project-main/main.tf
@@ -1,10 +1,45 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0"
-    }
+    kubernetes = {
+        source = "hashicorp/kubernetes"
+        version = ">= 2.23.0"
+      }
   }
 }
 
-provider "docker" {}
+provider "kubernetes" {
+  config_path = "/media/sf_terre_folders/kubeconfig"
+}
+resource "kubernetes_manifest" "db-data-pvc" {
+  manifest = yamldecode(file("${path.module}/k8s-manifests/db-data-pvc.yaml"))
+}
+resource "kubernetes_manifest" "pgsql-deployment" {
+  manifest = yamldecode(file("${path.module}/k8s-manifests/pgsql-deployment.yaml"))
+}
+resource "kubernetes_manifest" "pgsql-service" {
+  manifest = yamldecode(file("${path.module}/k8s-manifests/pgsql-service.yaml"))
+}
+resource "kubernetes_manifest" "redis-deployment" {
+  manifest = yamldecode(file("${path.module}/k8s-manifests/redis-deployment.yaml"))
+}
+resource "kubernetes_manifest" "result-deployment" {
+  manifest = yamldecode(file("${path.module}/k8s-manifests/result-deployment.yaml"))
+}
+resource "kubernetes_manifest" "redis-service" {
+  manifest = yamldecode(file("${path.module}/k8s-manifests/redis-service.yaml"))
+}
+resource "kubernetes_manifest" "result-service" {
+  manifest = yamldecode(file("${path.module}/k8s-manifests/result-service.yaml"))
+}
+resource "kubernetes_manifest" "seed-job" {
+  manifest = yamldecode(file("${path.module}/k8s-manifests/seed-job.yaml"))
+}
+resource "kubernetes_manifest" "vote-deployment" {
+  manifest = yamldecode(file("${path.module}/k8s-manifests/vote-deployment.yaml"))
+}
+resource "kubernetes_manifest" "vote-service" {
+  manifest = yamldecode(file("${path.module}/k8s-manifests/vote-service.yaml"))
+}
+resource "kubernetes_manifest" "worker-deployment" {
+  manifest = yamldecode(file("${path.module}/k8s-manifests/worker-deployment.yaml"))
+}
--- project-main/network.tf
+++
@@ -1,7 +0,0 @@
-resource "docker_network" "front_tier" {
-  name = "front-tier"
-}
-
-resource "docker_network" "back_tier" {
-  name = "back-tier"
-}
