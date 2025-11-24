a6b93738cf5a06ddd83c6f56c6fe0fc37d9ddb28
Julien <julien.millionbalard@lilo.org>
2025-11-24 17:53:10 +0100

    step3.... bog Il faut juste avoir une archi simple qui fonctionne même manuellement Le mdp se change bien par env L'ip faut hackjack le service/DNS de k8s Le but final est d'avoir une VM redis qui se lance manuellement sur terra + redis install manual + deploy cluster

--- project-main/main.tf
+++ project-main/main.tf
@@ -1,15 +1,16 @@
 terraform {
   required_providers {
     kubernetes = {
-        source = "hashicorp/kubernetes"
-        version = ">= 2.23.0"
-      }
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.23.0"
+    }
   }
 }
 
 provider "kubernetes" {
   config_path = "/media/sf_terre_folders/kubeconfig"
 }
+
 resource "kubernetes_manifest" "db-data-pvc" {
   manifest = yamldecode(file("${path.module}/k8s-manifests/db-data-pvc.yaml"))
 }
@@ -19,14 +20,8 @@ resource "kubernetes_manifest" "pgsql-deployment" {
 resource "kubernetes_manifest" "pgsql-service" {
   manifest = yamldecode(file("${path.module}/k8s-manifests/pgsql-service.yaml"))
 }
-resource "kubernetes_manifest" "redis-deployment" {
-  manifest = yamldecode(file("${path.module}/k8s-manifests/redis-deployment.yaml"))
-}
 resource "kubernetes_manifest" "result-deployment" {
   manifest = yamldecode(file("${path.module}/k8s-manifests/result-deployment.yaml"))
-}
-resource "kubernetes_manifest" "redis-service" {
-  manifest = yamldecode(file("${path.module}/k8s-manifests/redis-service.yaml"))
 }
 resource "kubernetes_manifest" "result-service" {
   manifest = yamldecode(file("${path.module}/k8s-manifests/result-service.yaml"))
@@ -42,4 +37,19 @@ resource "kubernetes_manifest" "vote-service" {
 }
 resource "kubernetes_manifest" "worker-deployment" {
   manifest = yamldecode(file("${path.module}/k8s-manifests/worker-deployment.yaml"))
+}
+
+
+/*
+resource "kubernetes_manifest" "redis-deployment" {
+  manifest = yamldecode(file("${path.module}/k8s-manifests/redis-deployment.yaml"))
+}
+*/
+// remote_state
+resource "kubernetes_manifest" "redis-service" {
+  manifest = yamldecode(file("${path.module}/k8s-manifests/redis-service.yaml"))
+}
+
+resource "kubernetes_manifest" "redis-endpoints" {
+  manifest = yamldecode(file("${path.module}/k8s-manifests/redis-endpoints.yaml"))
 }
